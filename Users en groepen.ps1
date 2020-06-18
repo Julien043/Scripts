a9ebf312024b01a49be979fa948d8fd6936fbcd3
Julien043 <julien.vandenheuvel@student.fontys.nl>
2020-06-18 13:11:04 +0200

    Toevoegen Powershell command

---
+++ Users en groepen.ps1
@@ -0,0 +1,37 @@
+﻿foreach ($line in Get-Content "C:\temp\Users.txt")
+{
+    $info = $line -split ";"
+
+    $Name = $info[0]
+    $GivenName = $info[1]
+    $oupath = $info[2]
+    $descrption = $info[3]
+    $UserPrincipalName = $info[4]
+
+    New-ADUser -name $Name `
+    -GivenName $GivenName `
+    -Path $oupath `
+    -Description $descrption `
+    -Userprincipalname $UserPrincipalName `
+    -ChangePasswordAtLogon 1 `
+    -Enabled 1 `
+    -AccountPassword (ConvertTo-SecureString "P@$$w0rd123" -AsPlainText -Force) `
+
+}
+
+foreach ($line in Get-Content "C:\temp\groups.txt")
+{
+
+    $info = $line -split ";"
+
+    $group = $info[0]
+    $oupath = $info[1]
+    $grouptype = $info[2]
+
+    New-ADGroup -Name $group -Path $oupath -GroupScope $grouptype
+
+
+
+}
+
+Import-Csv C:\temp\Group.csv.txt | ForEach-Object {Add-ADGroupMember -Identity $_.groupname -Members $_.username}
